cae76f9c147efbe30dd19e2f699cd55d2455cbf3
soojinoh <thensj@hanmail.net>
2017-07-02 05:54:10 +0000

    add devise views

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,4 +1,34 @@
 class BoardController < ApplicationController
   def index
   end
+  
+  def new_post
+  end
+  
+  def create_post
+  end
+  
+  def edit_post
+  end
+  
+  def update_post
+  end
+
+  def destroy_post
+  end
+  
+  def new_comment
+  end
+  
+  def create_comment
+  end
+  
+  def edit_comment
+  end
+  
+  def update_comment
+  end
+  
+  def destroy_comment
+  end
 end
